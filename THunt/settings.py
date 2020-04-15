2e19a18e5c695b26764959cf869272f1be55515c
arkaanfast <47567406+arkaanfast@users.noreply.github.com>
2020-04-15 23:49:08 +0530

    updated settings

--- THunt/settings.py
+++ THunt/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '+y5nx-)m0rpsu=6=n3iw8m#av7%-x!ns*v*4t@g=!_)-m4504&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -122,8 +122,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'statics'),
-]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'statics'),
+# ]
 
